4e062e861b4bf42182c4ef0bfd14dc6105c19713
Christian <christianlindeneg@gmail.com>
2021-03-07 18:58:52 +0100

    use env var for api key

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 
 
+if (typeof process.env.REACT_APP_GOOGLE_MAP_API_KEY === 'undefined') {
+  throw new Error('Please specify a \'REACT_APP_GOOGLE_MAP_API_KEY\' env var for Google Maps.');
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
